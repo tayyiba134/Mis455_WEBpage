2b17a2341298c38f2c7d74398a4efdcf006c3c27
tayyiba134 <thaibabng@gmail.com>
2025-03-22 21:02:04 +0600

    Your commit message here

--- script.js
+++ script.js
@@ -2,3 +2,13 @@ function showAlert() {
     alert("Welcome to our store!");
 }
 
+function validateForm() {
+    var name = document.getElementById("name").value;
+    var email = document.getElementById("email").value;
+    if (name == "" || email == "") {
+        alert("Please fill in all fields");
+        return false;
+    }
+    alert("Message sent!");
+    return true;
+}
